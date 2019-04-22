ce27b718b95d9878a7eb685ab846001cfe84ab35
Christopher Lin <powertothepenguins@gmail.com>
2019-04-22 14:48:58 -0700

    Add d3 tutorial

---
+++ d3.js
@@ -0,0 +1,25 @@
+let data = [4, 8, 15, 16, 23, 42];
+
+let width = 420;
+let barHeight = 20;
+let x = d3.scaleLinear().domain([0, d3.max(data)]).range([0, width]);
+
+let divChart = d3.select('#bar-chart');
+divChart.selectAll('div')
+    .data(data)
+  .enter().append('div')
+    .style('width', function(d) { return x(d) + 'px'; })
+    .text(function(d) { return d; });
+
+let chart = d3.select('.chart').attr('width', width).attr('height', barHeight * data.length);
+let bar = chart.selectAll('g').data(data).enter().append('g');
+
+// Position each bar at a different vertical point.
+bar.attr('transform', function(d, i) { return 'translate(0,' + i * barHeight + ')'; });
+
+bar.append('rect').attr('width', x).attr('height', barHeight - 1);
+bar.append('text')
+  .attr('x', function(d) { return x(d) - 3; })
+  .attr('y', barHeight / 2)
+  .attr('dy', '.35em') // Vertically center the text within the bar.
+  .text(function(d) { return d; });
